df07d205c469bd54826b66c44fa1a3925b8d6f27
alorenzo.md@gmail.com <alorenzo.md@gmail.com>
2017-11-08 08:29:01 +0100

    Fix static initialization order fiasco

--- source/lexer.cpp
+++ source/lexer.cpp
@@ -1,10 +1,10 @@
 #include "calculate.h"
 
-const char* const calculate_left_token = Lexer::left.c_str();
-const char* const calculate_right_token = Lexer::right.c_str();
-const char* const calculate_decimal_token = Lexer::decimal.c_str();
-const char* const calculate_separator_token = Lexer::separator.c_str();
-const char* const calculate_number_regex = Lexer::number_regex.pattern.c_str();
-const char* const calculate_name_regex = Lexer::name_regex.pattern.c_str();
-const char* const calculate_symbol_regex = Lexer::symbol_regex.pattern.c_str();
-const char* const calculate_tokenizer_regex = Lexer::tokenizer_regex.pattern.c_str();
+const char* const calculate_left_token = Lexer::left().c_str();
+const char* const calculate_right_token = Lexer::right().c_str();
+const char* const calculate_decimal_token = Lexer::decimal().c_str();
+const char* const calculate_separator_token = Lexer::separator().c_str();
+const char* const calculate_number_regex = Lexer::number_regex().pattern.c_str();
+const char* const calculate_name_regex = Lexer::name_regex().pattern.c_str();
+const char* const calculate_symbol_regex = Lexer::symbol_regex().pattern.c_str();
+const char* const calculate_tokenizer_regex = Lexer::tokenizer_regex().pattern.c_str();
