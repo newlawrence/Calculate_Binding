0c523f98594c68430892b8e7b46b0fbe5426cd0a
newlawrence <alorenzo.md@gmail.com>
2017-12-03 20:29:37 +0100

    Parser refactored

--- source/parser.cpp
+++ source/parser.cpp
@@ -1,174 +1,174 @@
 #include "calculate.h"
 
 
-cParser _calculate_get_parser() {
+cParser calculate_get_parser() {
     return Factory<Parser, cParser>::get();
 }
 
-cParser _calculate_get_default_parser() {
+cParser calculate_get_default_parser() {
     return Factory<Parser, cParser, calculate::DefaultParser>::get();
 }
 
 
-double _calculate_cast(cError error, cParser parser, const char* expression) {
+double calculate_cast(cError error, cParser parser, const char* expr) {
     return evaluate(
-        [parser, expression]() {
-            return reinterpret_cast<Parser*>(parser)->cast(expression);
+        [parser, expr]() {
+            return reinterpret_cast<Parser*>(parser)->cast(expr);
         },
         error
     );
 }
 
-void _calculate_to_string(
+void calculate_to_string(
     cError error,
     cParser parser,
-    double value,
-    char* expression,
-    size_t length
+    double val,
+    char* expr,
+    size_t len
 ) {
     write(
-        [parser, value]() {
-            return reinterpret_cast<Parser*>(parser)->to_string(value);
+        [parser, val]() {
+            return reinterpret_cast<Parser*>(parser)->to_string(val);
         },
-        expression,
-        length,
+        expr,
+        len,
         error
     );
 }
 
 
-cExpression _calculate_create_node(
+cExpression calculate_create_node(
     cError error,
     cParser parser,
     const char* token,
     cNodes nodes,
-    const char* variables
+    const char* vars
 ) {
     return Factory<Expression, cExpression>::create(
-        [parser, token, variables, nodes]() {
+        [parser, token, vars, nodes]() {
             return reinterpret_cast<Parser*>(parser)->create_node(
                 token,
                 *reinterpret_cast<Nodes*>(nodes),
-                from_string(variables)
+                from_string(vars)
             );
         },
         error
     );
 }
 
-cExpression _calculate_from_value(cError error, cParser parser, double value) {
+cExpression calculate_from_value(cError error, cParser parser, double val) {
     return Factory<Expression, cExpression>::create(
-        [parser, value]() {
-            return reinterpret_cast<Parser*>(parser)->create_node(value);
+        [parser, val]() {
+            return reinterpret_cast<Parser*>(parser)->create_node(val);
         },
         error
     );
 }
 
-cExpression _calculate_from_infix(
+cExpression calculate_from_infix(
     cError error,
     cParser parser,
-    const char* expression,
-    const char* variables
+    const char* expr,
+    const char* vars
 ) {
     return Factory<Expression, cExpression>::create(
-        [parser, expression, variables]() {
+        [parser, expr, vars]() {
             return reinterpret_cast<Parser*>(parser)->
-                from_infix(expression, from_string(variables));
+                from_infix(expr, from_string(vars));
         },
         error
     );
 }
 
-cExpression _calculate_from_postfix(
+cExpression calculate_from_postfix(
     cError error,
     cParser parser,
-    const char* expression,
-    const char* variables
+    const char* expr,
+    const char* vars
 ) {
     return Factory<Expression, cExpression>::create(
-        [parser, expression, variables]() {
+        [parser, expr, vars]() {
             return reinterpret_cast<Parser*>(parser)->
-                from_postfix(expression, from_string(variables));
+                from_postfix(expr, from_string(vars));
         },
         error
     );
 }
 
-cExpression _calculate_parse(
+cExpression calculate_parse(
     cError error,
     cParser parser,
-    const char* expression
+    const char* expr
 ) {
     return Factory<Expression, cExpression>::create(
-        [parser, expression]() {
-            return reinterpret_cast<Parser*>(parser)->parse(expression);
+        [parser, expr]() {
+            return reinterpret_cast<Parser*>(parser)->parse(expr);
         },
         error
     );
 }
 
 
-cExpression _calculate_new_variables(
+cExpression calculate_new_variables(
     cError error,
     cParser parser,
-    cExpression expression,
-    const char* variables
+    cExpression expr,
+    const char* vars
 ) {
     return Factory<Expression, cExpression>::create(
-        [parser, expression, variables]() {
-            return reinterpret_cast<Parser*>(parser)->variables(
-                *reinterpret_cast<Expression*>(expression),
-                from_string(variables)
+        [parser, expr, vars]() {
+            return reinterpret_cast<Parser*>(parser)->vars(
+                *reinterpret_cast<Expression*>(expr),
+                from_string(vars)
             );
         },
         error
     );
 }
 
-cExpression _calculate_optimize(cParser parser, cExpression expression) {
+cExpression calculate_optimize(cParser parser, cExpression expr) {
     return Factory<Expression, cExpression>::get(
         reinterpret_cast<Parser*>(parser)->
-            optimize(*reinterpret_cast<Expression*>(expression))
+            optimize(*reinterpret_cast<Expression*>(expr))
     );
 }
 
-cExpression _calculate_replace(
+cExpression calculate_replace(
     cError error,
     cParser parser,
-    cExpression expression,
+    cExpression expr,
     size_t branch,
     cExpression another,
-    const char* variables
+    const char* vars
 ) {
     return Factory<Expression, cExpression>::create(
-        [parser, expression, branch, another, variables]() {
+        [parser, expr, branch, another, vars]() {
             return reinterpret_cast<Parser*>(parser)->
                 replace(
-                    *reinterpret_cast<Expression*>(expression),
+                    *reinterpret_cast<Expression*>(expr),
                     branch,
                     *reinterpret_cast<Expression*>(another),
-                    from_string(variables)
+                    from_string(vars)
                 );
         },
         error
     );
 }
 
-cExpression _calculate_substitute(
+cExpression calculate_substitute(
     cError error,
     cParser parser,
-    cExpression expression,
+    cExpression expr,
     const char* variable,
-    double value
+    double val
 ) {
     return Factory<Expression, cExpression>::create(
-        [parser, expression, variable, value]() {
+        [parser, expr, variable, val]() {
             return reinterpret_cast<Parser*>(parser)->
                 substitute(
-                    *reinterpret_cast<Expression*>(expression),
+                    *reinterpret_cast<Expression*>(expr),
                     variable,
-                    value
+                    val
                 );
         },
         error
