92181cde45d6e740bc2c7b78ee0813a4e512d91f
newlawrence <alorenzo.md@gmail.com>
2017-12-03 20:26:21 +0100

    Handlers refactored

--- source/handler.cpp
+++ source/handler.cpp
@@ -1,64 +1,64 @@
 #include "calculate.h"
 
 
-cError _calculate_get_error() {
+cError calculate_get_error() {
     return Factory<Error, cError>::get();
 }
 
-int _calculate_status(cError error) {
+int calculate_status(cError error) {
     return static_cast<int>(reinterpret_cast<Error*>(error)->status);
 }
 
-void _calculate_message(cError error, char* message, size_t length) {
-    write(message, reinterpret_cast<Error*>(error)->message.c_str(), length);
+void calculate_message(cError error, char* message, size_t len) {
+    write(message, reinterpret_cast<Error*>(error)->message.c_str(), len);
 }
 
 
-int _calculate_check_parser(cParser resource) {
+int calculate_check_parser(cParser resource) {
     return find<Parser, cParser>(resource);
 }
 
-int _calculate_check_nodes(cNodes resource) {
+int calculate_check_nodes(cNodes resource) {
     return find<Nodes, cNodes>(resource);
 }
 
-int _calculate_check_expression(cExpression resource) {
+int calculate_check_expression(cExpression resource) {
     return find<Expression, cExpression>(resource);
 }
 
-int _calculate_check_function(cFunction resource) {
+int calculate_check_function(cFunction resource) {
     return find<Function, cFunction>(resource);
 }
 
-int _calculate_check_operator(cOperator resource) {
+int calculate_check_operator(cOperator resource) {
     return find<Operator, cOperator>(resource);
 }
 
-int _calculate_check_error(cError resource) {
+int calculate_check_error(cError resource) {
     return find<Error, cError>(resource);
 }
 
 
-void _calculate_free_parser(cParser resource) {
+void calculate_free_parser(cParser resource) {
     return free<Parser, cParser>(resource);
 }
 
-void _calculate_free_nodes(cNodes resource) {
+void calculate_free_nodes(cNodes resource) {
     return free<Nodes, cNodes>(resource);
 }
 
-void _calculate_free_expression(cExpression resource) {
+void calculate_free_expression(cExpression resource) {
     return free<Expression, cExpression>(resource);
 }
 
-void _calculate_free_function(cFunction resource) {
+void calculate_free_function(cFunction resource) {
     return free<Function, cFunction>(resource);
 }
 
-void _calculate_free_operator(cOperator resource) {
+void calculate_free_operator(cOperator resource) {
     return free<Operator, cOperator>(resource);
 }
 
-void _calculate_free_error(cError resource) {
+void calculate_free_error(cError resource) {
     return free<Error, cError>(resource);
 }
